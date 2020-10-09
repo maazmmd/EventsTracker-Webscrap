3724ed6c799862753c7cb48050633e9dc40b755f
Mohammed Maaz <mohammedmaaz@Mohammeds-MacBook-Pro.local>
2020-10-09 02:15:33 -0400

    Back end and git ignore changes

--- expensetracker/src/main/java/in/bushansirgur/expensetracker/model/Expense.java
+++ expensetracker/src/main/java/in/bushansirgur/expensetracker/model/Expense.java
@@ -12,18 +12,51 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="tbl_expenses")
+@Table(name = "tbl_expenses")
 @Setter
 @Getter
 public class Expense {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
-	
+
 	private String expense;
-	
+
 	private String description;
-	
+
 	private BigDecimal amount;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getExpense() {
+		return expense;
+	}
+
+	public void setExpense(String expense) {
+		this.expense = expense;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
 }
